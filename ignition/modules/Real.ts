719d29f8e0fde96dd277ef597a13428c8824cd32
Meenakshi19-cse <meenakshiharish2006@gmail.com>
2026-01-08 23:59:10 +0530

    Added

--- ignition/modules/Real.ts
+++
@@ -1,8 +0,0 @@
-import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
-
-const RealModule = buildModule("RealModule",(m)=>{
-    const real = m.contract("RealEstate");
-    return {real};
-})
-
-export default RealModule;
